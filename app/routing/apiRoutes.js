7373925e22b8c6e6ffed93a2ef56eeb8173eaa59
Jonathan Behar <jdb443@stern.nyu.edu>
2019-07-23 23:53:07 -0400

    update

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,42 @@
+var path = require('path');
+
+var friends = require('../data/friends.js');
+
+module.exports = function(app) {
+
+	app.get('/api/friends', function(req, res) {
+		res.json(friends);
+	});
+
+
+	app.post('/api/friends', function(req, res) {
+
+		var userInput = req.body;
+
+		var userResponses = userInput.scores;
+
+		var matchName = '';
+		var matchImage = '';
+		var totalDifference = 10000;
+
+
+		for (var i = 0; i < friends.length; i++) {
+
+			var diff = 0;
+			for (var j = 0; j < userResponses.length; j++) {
+				diff += Math.abs(friends[i].scores[j] - userResponses[j]);
+			}
+
+			if (diff < totalDifference) {
+
+				totalDifference = diff;
+				matchName = friends[i].name;
+				matchImage = friends[i].photo;
+			}
+		}
+
+		friends.push(userInput);
+
+		res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
+	});
+};
---
+++ server.js
@@ -0,0 +1,20 @@
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+var PORT = process.env.PORT;
+
+app.use(express.static(path.join(__dirname, './app/public')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+
+app.listen(PORT, function() {
+  console.log('Friend Finder app is listening on PORT: ' + PORT);
+});
