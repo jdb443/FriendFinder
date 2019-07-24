c6bfc1a1aaaa6f0b79a6c4889ff3cd9392cf7dcf
Jonathan Behar <jdb443@stern.nyu.edu>
2019-07-23 23:39:01 -0400

    draft

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the players data
+var friendsFinderArray = [
+	{
+		name: 'LeBron James',
+		photo: 'https://cdnph.upi.com/svc/sv/upi/6021547669776/2019/1/127b3c18cb2ae973bc292527ae956fb4/Lakers-LeBron-James-to-miss-at-least-two-more-games.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Michael Jordan',
+		photo: 'https://imagesvc.timeincapp.com/v3/fan/image?url=https://dawindycity.com/wp-content/uploads/getty-images/2018/01/262746-michael-jordan.jpg.jpg&',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Muhammad Ali',
+		photo: 'https://img.washingtonpost.com/wp-apps/imrs.php?src=https://img.washingtonpost.com/rf/image_960w/2010-2019/Wires/Images/2016-06-06/AP/Obit_Muhammad_Ali_-66c43.jpg&w=400',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Michael Phelps',
+		photo: 'https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-s3.si.com%2Fs3fs-public%2Fstyles%2Fmarquee_large_2x%2Fpublic%2F%255Bcurrent-date%253Acustom%253AY%255D%2F%255Bcurrent-date%253Acustom%253Am%255D%2F%255Bcurrent-date%253Acustom%253Ad%255D%2Fmichael-phelps-100-butterfly-win-video.jpg%3Fitok%3DAfh5HfEV&w=1000&q=70',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Usain Bolt',
+		photo: 'https://media.self.com/photos/57d88409f71ce8751f6b46cd/4:3/w_728,c_limit/usainbolt.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Babe Ruth',
+		photo: 'https://static.politico.com/dims4/default/cbccf60/2147483647/resize/1160x%3E/quality/90/?url=https%3A%2F%2Fstatic.politico.com%2Fee%2Fa7%2F559abc2e43c6b0a0bb88f670a767%2F160925-babe-ruth-ap-98101401969.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: "Shaquille O'Neal",
+		photo: 'https://thebasketballsociety.files.wordpress.com/2015/02/tumblr_m5vomhqgcm1qm9rypo1_1280.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Jesse Owens',
+		photo: 'https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-s3.si.com%2Fs3fs-public%2Fsi%2Fdam%2Fassets%2F13%2F09%2F12%2F130912114237-1936-jesse-owens-icon-12086673-single-image-cut.jpg&w=1000&q=70',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Nadia Comăneci',
+		photo: 'https://cdn-s3.si.com/s3fs-public/nadia-comaneci-vaultlead.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Billie Jean King',
+		photo: 'https://www.history.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cg_faces:center%2Cq_auto:good%2Cw_620/MTU3ODc5MDg0MDE1NDk0ODc5/virginia-slims-championship.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsFinderArray;
